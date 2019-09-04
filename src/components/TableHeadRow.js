b8b53903c89d8373f0623d3e8fbd73d89b52011a
patorjk <patorjk@gmail.com>
2019-09-04 14:08:41 -0400

    lib adding

--- src/components/TableHeadRow.js
+++ src/components/TableHeadRow.js
@@ -2,30 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import TableRow from '@material-ui/core/TableRow';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/styles';
+import { useDrop } from 'react-dnd';
 
-const defaultHeadRowStyles = {
-  root: {},
-};
+const useStyles = makeStyles(
+  theme => ({
+    root: {},
+  }),
+  { name: 'MUIDataTableHeadRow' },
+);
 
-class TableHeadRow extends React.Component {
-  static propTypes = {
-    /** Extend the style applied to components */
-    classes: PropTypes.object,
-  };
+function TableHeadRow(props) {
+  const classes = useStyles();
 
-  render() {
-    const { classes } = this.props;
+  const [, drop] = useDrop({
+    accept: 'TABLE_HEAD_CELL_LABEL',
+    drop(item, monitor) {
+      const delta = monitor.getDifferenceFromInitialOffset();
+      const left = Math.round(item.left + delta.x);
+      const top = Math.round(item.top + delta.y);
+      console.log(delta, left, top);
+      return undefined;
+    },
+  });
 
-    return (
-      <TableRow
-        className={classNames({
-          [classes.root]: true,
-        })}>
-        {this.props.children}
-      </TableRow>
-    );
-  }
+  return (
+    <TableRow
+      className={classNames({
+        [classes.root]: true,
+      })}>
+      {this.props.children}
+    </TableRow>
+  );
 }
 
-export default withStyles(defaultHeadRowStyles, { name: 'MUIDataTableHeadRow' })(TableHeadRow);
+TableHeadRow.propTypes = {
+  /** Extend the style applied to components */
+  classes: PropTypes.object,
+};
+
+export default TableHeadRow;
